ae0ba0fe899e35aa3fe2363596e1fd5fae069923
sanchez <sanchez@aaa.com>
2016-01-19 13:58:28 +0100

    cw4.1

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,5 +1,22 @@
-$(function(){ $('.opis h2').mouseover(function ()
-{
-	alert('Witaj');
-});
+$(function(){
+	$('#czerwony').mouseover(function(){
+		$('p').css('background','red');
+	});
+	$('#czerwony').mouseout(function(){
+		$('p').css('background','white');
+	});
+
+	$('#zielony').mouseover(function(){
+		$('p').css('background','green');
+	});
+	$('#zielony').mouseout(function(){
+		$('p').css('background','white');
+	});
+
+	$('#niebieski').mouseover(function(){
+		$('p').css('background','blue');
+	});
+	$('#niebieski').mouseout(function(){
+		$('p').css('background','white');
+	});
 });
